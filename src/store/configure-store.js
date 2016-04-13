61077776131ae1cbc1e47519196c70dd2f3a5568
Dominic Lind <linddominic@gmail.com>
2016-04-13 06:28:03 +0200

    + added redux remote dev, local remote dev server as dep

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,11 +1,31 @@
-import {createStore, applyMiddleware} from 'redux';
+import { Platform } from 'react-native';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/index';
+import devTools from 'remote-redux-devtools';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+let configureStore;
+
+let enhancer;
+if (__DEV__) {
+  enhancer = compose(
+    applyMiddleware(thunkMiddleware),
+    devTools({
+      name: Platform.OS,
+      hostname: 'localhost',
+      port: 8000
+    })
+  );
+} else {
+  enhancer = applyMiddleware(thunkMiddleware);
+}
 
 export default function configureStore(initialState) {
-    const store = createStoreWithMiddleware(rootReducer, initialState);
-
-    return store;
+  const store = createStore(rootReducer, initialState, enhancer);
+  if (module.hot) {
+    module.hot.accept(() => {
+      store.replaceReducer(require('../reducers/index'));
+    });
+  }
+  return store;
 }
